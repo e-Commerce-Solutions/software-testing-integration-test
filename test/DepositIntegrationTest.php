b478f642effbe390588f2a7ab0664360aee3ee3f
tinnakrit <tinnkrit.n@gmail.com>
2021-11-21 01:15:03 +0700

    Update DepositIntegrationTest.php

--- test/DepositIntegrationTest.php
+++ test/DepositIntegrationTest.php
@@ -20,7 +20,7 @@ class DepositServiceStubAuthenticationSaveTransaction extends DepositService {
     );
   }
 }
-final class RandomTesting extends TestCase {
+final class IntegrationTesting extends TestCase {
   // Driver + Deposit + Service A + Stub Service B | invalid
   function testITDP02_01() {
     $deposit_service = new DepositServiceAuthenticationStubSaveTransaction('1111111111');
@@ -34,6 +34,21 @@ final class RandomTesting extends TestCase {
     $result = $deposit_service->deposit(50000);
     $this->assertEquals(false, $result['isError']);
   }
+  /*
+    There is no error returned because MySQL does not return any errors on UPDATE
+    ( except the syntax schema-related ones of course ).
+    You can go into phpMyAdmin because I guess you're using it,
+    and run the same update with some dummy values,
+    you'll see the result is a success with 0 affected rows.
+    The update still happened successfully and it affected no rows because none met the requirements.
+    Just so you can get a clearer idea of what happened, you can look for the number of affected rows using the following:
+    ````
+    if ($conn->query($query) === TRUE) {
+      echo ''.$message_status_array[$key].':'.$value.':OK';
+      echo $conn->affected_rows . ' affected';
+    }
+    ````
+  */
   // Driver + Deposit + Stub Service A + Service B | invalid
   function testITDP03_01() {
     $deposit_service = new DepositServiceStubAuthenticationSaveTransaction('1111111111');
